cc3a663f8a3d7279b5c253fa4b1d2fb3aa4c764a
lbreede <67719520+lbreede@users.noreply.github.com>
2024-12-02 09:54:45 -0800

    solved day 2 part 1

---
+++ src/bin/02.rs
@@ -0,0 +1,68 @@
+use adv_code_2024::*;
+use anyhow::*;
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use itertools::Itertools;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const DAY: &str = "02";
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+const TEST: &str = "\
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+";
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    //region Part 1
+    println!("=== Part 1 ===");
+
+    fn part1<R: BufRead>(reader: R) -> Result<usize> {
+        let answer = reader
+            .lines()
+            .map_while(Result::ok)
+            .map(|line| {
+                line.split_whitespace()
+                    .map(|n| n.parse::<i32>().expect("should only have numbers"))
+                    .tuple_windows()
+                    .map(|(a, b)| a - b)
+                    .collect::<Vec<i32>>()
+            })
+            .filter(|v| {
+                (v.iter().all(|&x| x > 0) || v.iter().all(|&x| x < 0))
+                    && v.iter().all(|&x| x.abs() >= 1 && x.abs() <= 3)
+            })
+            .count();
+        Ok(answer)
+    }
+
+    assert_eq!(2, part1(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {}", result);
+    //endregion
+
+    //region Part 2
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        Ok(0)
+    }
+
+    assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
+    //endregion
+
+    Ok(())
+}
