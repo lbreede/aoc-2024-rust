ec62a75e79d2e2b3d336b05e68e07e9df86f6d13
lbreede <67719520+lbreede@users.noreply.github.com>
2024-12-05 09:26:38 -0800

    solved day 5 part 2 (phew)

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -47,8 +47,8 @@ fn main() -> Result<()> {
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
         let mut parse_page_nums = true;
-        let mut page_ordering_rules: Vec<[u32; 2]> = Vec::new();
-        let mut pages_to_produce: Vec<Vec<u32>> = Vec::new();
+        let mut page_ordering_rules: Vec<[usize; 2]> = Vec::new();
+        let mut pages_to_produce: Vec<Vec<usize>> = Vec::new();
         for line in reader.lines().map_while(Result::ok) {
             if line.is_empty() {
                 parse_page_nums = false;
@@ -56,11 +56,15 @@ fn main() -> Result<()> {
             }
             if parse_page_nums {
                 let (x, y) = line.split_once("|").unwrap();
-                let x: u32 = x.parse()?;
-                let y: u32 = y.parse()?;
+                let x: usize = x.parse()?;
+                let y: usize = y.parse()?;
                 page_ordering_rules.push([x, y]);
             } else {
-                pages_to_produce.push(line.split(",").map(|x| x.parse::<u32>().unwrap()).collect())
+                pages_to_produce.push(
+                    line.split(",")
+                        .map(|x| x.parse::<usize>().unwrap())
+                        .collect(),
+                )
             }
         }
 
@@ -69,20 +73,17 @@ fn main() -> Result<()> {
             let mut k = 1; // FIXME: I HATE THIS
             'outer: for (i, &page) in pages.iter().enumerate() {
                 for &next_page in pages[i + 1..].iter() {
-                    for &rule in &page_ordering_rules {
-                        if rule == [next_page, page] {
-                            k = 0;
-                            break 'outer;
-                        }
+                    if page_ordering_rules.contains(&[next_page, page]) {
+                        k = 0;
+                        break 'outer;
                     }
                 }
             }
             answer += pages.get(pages.len().div_euclid(2)).unwrap() * k;
         }
-        Ok(answer as usize)
+        Ok(answer)
     }
 
-    // TODO: Set the expected answer for the test input
     assert_eq!(143, part1(BufReader::new(EXAMPLE.as_bytes()))?);
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
@@ -91,17 +92,68 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(EXAMPLE.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let mut parse_page_nums = true;
+        let mut page_ordering_rules: Vec<[usize; 2]> = Vec::new();
+        let mut pages_to_produce: Vec<Vec<usize>> = Vec::new();
+        for line in reader.lines().map_while(Result::ok) {
+            if line.is_empty() {
+                parse_page_nums = false;
+                continue;
+            }
+            if parse_page_nums {
+                let (x, y) = line.split_once("|").unwrap();
+                let x: usize = x.parse()?;
+                let y: usize = y.parse()?;
+                page_ordering_rules.push([x, y]);
+            } else {
+                pages_to_produce.push(
+                    line.split(",")
+                        .map(|x| x.parse::<usize>().unwrap())
+                        .collect(),
+                )
+            }
+        }
+
+        let mut answer = 0;
+        for mut pages in pages_to_produce {
+            let mut k = 0;
+
+            // We loop over a range, rather than the vec itself since we want to mutate the vec
+            // while looping over it.
+            for i in 0..pages.len() - 1 {
+                'inner: loop {
+                    for j in (i + 1)..pages.len() {
+                        // If `page_order_rules` contains the incorrect ordering, we swap the
+                        // incorrect indices, set `k` to 1 to indicate that these pages were changed
+                        // and finally `continue` the inner loop to re-check the ordering from the
+                        // current `i`, not from the very beginning.
+                        if page_ordering_rules.contains(&[pages[j], pages[i]]) {
+                            pages.swap(i, j);
+                            k = 1;
+                            continue 'inner;
+                        }
+                        // If `j` reaches the end of pages, that means `i` is in the correct place
+                        // We can therefore break out of the inner loop, increment `i` and continue
+                        // looping.
+                        if j == pages.len() - 1 {
+                            break 'inner;
+                        }
+                    }
+                }
+            }
+            answer += pages.get(pages.len().div_euclid(2)).unwrap() * k;
+        }
+        Ok(answer)
+    }
+
+    assert_eq!(123, part2(BufReader::new(EXAMPLE.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
