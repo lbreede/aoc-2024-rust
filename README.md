349383c09435559bbba8150e6deea8ba78d40ec4
lbreede <67719520+lbreede@users.noreply.github.com>
2024-12-09 10:15:37 -0800

    solved day 9

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Advent of Code 2024 in Rust :christmas_tree::crab:
 
-My solutions for [Advent of Code](https://adventofcode.com/2024) 2024 in [Rust](https://www.rust-lang.org/).
+My solutions for [Advent of Code](https://adventofcode.com/2024) 2024
+in [Rust](https://www.rust-lang.org/).
 
 I was encouraged to use in [RustRover](https://www.jetbrains.com/rust/) after
 reading [this blog post](https://blog.jetbrains.com/rust/2024/11/29/advent-of-code-in-rust-for-the-rest-of-us/)
@@ -18,6 +19,7 @@ by Vitaly Bragilevsky.
 |  6  | :star: | :star: |
 |  7  | :star: | :star: |
 |  8  | :star: | :star: |
+|  9  | :star: | :star: |
 
 ## Timings :stopwatch:
 
@@ -33,32 +35,42 @@ by Vitaly Bragilevsky.
 |  6  |  `516.75µs` |   `7.650 s` |
 |  7  |  `21.319ms` |   `1.302 s` |
 |  8  | `104.208µs` | `224.995µs` |
+|  9  | `669.350ms` |   `1.467 s` |
 
 ***
 
 # Template for solving Advent of Code puzzles in Rust with RustRover
 
-Read the [blog post](https://blog.jetbrains.com/rust/2024/11/29/advent-of-code-in-rust-for-the-rest-of-us/) that
+Read
+the [blog post](https://blog.jetbrains.com/rust/2024/11/29/advent-of-code-in-rust-for-the-rest-of-us/)
+that
 explains the structure and rationale behind this template.
 
 ## Usage
 
 1. Create a new project from the template repository:
     - Using GitHub’s templating feature: Simply click the Use this
-      template [button](https://github.com/new?template_name=advent-of-code-rust-template&template_owner=bravit) on the
-      repository page, create a new repository, and then open it in [RustRover](https://www.jetbrains.com/rust/) by
+      template [button](https://github.com/new?template_name=advent-of-code-rust-template&template_owner=bravit)
+      on the
+      repository page, create a new repository, and then open it
+      in [RustRover](https://www.jetbrains.com/rust/) by
       selecting *File | New | Project From Version Control…*.
-    - Adding the template to RustRover: You can integrate the template directly into RustRover and use the regular New
+    - Adding the template to RustRover: You can integrate the template directly into
+      RustRover and use the regular New
       Project wizard.
 
 2. Whenever you're ready to start solving a new day's puzzle:
-    - Open the `bin` folder, copy and paste the `NN.rs` file into it, and give it the corresponding name (`01.rs`,
+    - Open the `bin` folder, copy and paste the `NN.rs` file into it, and give it the
+      corresponding name (`01.rs`,
       `02.rs`, etc.).
-    - In the `input` folder, create and fill the input data file (`01.txt`, `02.txt`, etc.).
+    - In the `input` folder, create and fill the input data file (`01.txt`, `02.txt`,
+      etc.).
     - Fill in the `DAY` constant in the freshly created file.
-    - Run the current day's solution to check if it compiles (you can use the gutter icon next to the `main` function).
+    - Run the current day's solution to check if it compiles (you can use the gutter
+      icon next to the `main` function).
     - Fill in `<TEST-INPUT>`.
-    - Write the expected answer for the test data in the `assert_eq` statement in *Part 1*.
+    - Write the expected answer for the test data in the `assert_eq` statement in *Part
+      1*.
     - Now you're ready to write your solution in the `part1` function (inside `main`).
     - Use `Shift+F10` (Win/Linux) or `Ctrl-R` (macOS) to re-run the same program.
 
--- src/bin/09.rs
+++ src/bin/09.rs
@@ -2,7 +2,7 @@ use adv_code_2024::*;
 use anyhow::Result;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
-use itertools::Itertools;
+use itertools::all;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -18,25 +18,7 @@ fn main() -> Result<()> {
     println!("=== Part 1 ===");
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        let mut filesystem: Vec<Option<usize>> = Vec::new();
-        let mut id: usize = 0;
-        for (i, n) in reader
-            .bytes()
-            .map_while(Result::ok)
-            .map(|b| b - 48)
-            .enumerate()
-        {
-            if i % 2 == 0 {
-                for _ in 0..n {
-                    filesystem.push(Some(id));
-                }
-                id += 1
-            } else {
-                for _ in 0..n {
-                    filesystem.push(None);
-                }
-            }
-        }
+        let mut filesystem = parse_filesystem(reader);
         loop {
             let first_none = filesystem.iter().position(|x| x.is_none()).unwrap();
             let last_some = filesystem.iter().rposition(|x| x.is_some()).unwrap();
@@ -56,46 +38,46 @@ fn main() -> Result<()> {
 
     assert_eq!(1928, part1(BufReader::new(EXAMPLE.as_bytes()))?);
 
-    // FIXME: Uncomment before committing
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part1(input_file)?);
-    // println!("Result = {}", result);
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     //region Part 2
     println!("\n=== Part 2 ===");
 
     fn part2<R: BufRead>(reader: R) -> Result<usize> {
-        let mut filesystem: Vec<Option<usize>> = Vec::new();
-        let mut id: usize = 0;
-        for (i, n) in reader
-            .bytes()
-            .map_while(Result::ok)
-            .map(|b| b - 48)
-            .enumerate()
-        {
-            if i % 2 == 0 {
-                for _ in 0..n {
-                    filesystem.push(Some(id));
-                }
-                id += 1
-            } else {
-                for _ in 0..n {
-                    filesystem.push(None);
+        let mut filesystem = parse_filesystem(reader);
+        let id = filesystem.iter().filter_map(|x| *x).max().unwrap() + 1;
+
+        // We can skip 0 here since it should always be in its right place
+        for i in (1..id).rev() {
+            // Find the chunk of files with id == i
+            let chunk: Vec<usize> = filesystem
+                .iter()
+                .enumerate()
+                .skip_while(|(_, &x)| x != Some(i))
+                .take_while(|(_, &x)| x == Some(i))
+                .map(|(i, _)| i)
+                .collect();
+
+            // Loop from 0 to the beginning of the chunk
+            for j in 0..=chunk[0] - chunk.len() {
+                // Check if the chunk fits in the filesystem
+                if all(&filesystem[j..j + chunk.len()], |x| x.is_none()) {
+                    // Swap the chunk with the empty space
+                    for (k, &index) in chunk.iter().enumerate() {
+                        filesystem.swap(j + k, index);
+                    }
+                    break;
                 }
             }
         }
-
-        let chunks: Vec<Vec<Option<usize>>> = filesystem
-            .into_iter()
-            .chunk_by(|x| *x)
-            .into_iter()
-            .map(|(_, group)| group.collect())
-            .collect();
-
-        println!("{:?}", chunks);
-
-        Ok(0)
+        Ok(filesystem
+            .iter()
+            .enumerate()
+            .map(|(i, &x)| i * x.unwrap_or(0))
+            .sum())
     }
 
     assert_eq!(2858, part2(BufReader::new(EXAMPLE.as_bytes()))?);
@@ -106,4 +88,27 @@ fn main() -> Result<()> {
     //endregion
 
     Ok(())
+}
+
+fn parse_filesystem<R: BufRead>(reader: R) -> Vec<Option<usize>> {
+    let mut filesystem: Vec<Option<usize>> = Vec::new();
+    let mut id: usize = 0;
+    for (i, n) in reader
+        .bytes()
+        .map_while(Result::ok)
+        .map(|b| b - 48)
+        .enumerate()
+    {
+        if i % 2 == 0 {
+            for _ in 0..n {
+                filesystem.push(Some(id));
+            }
+            id += 1
+        } else {
+            for _ in 0..n {
+                filesystem.push(None);
+            }
+        }
+    }
+    filesystem
 }
